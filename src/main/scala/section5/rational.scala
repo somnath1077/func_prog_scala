150e5736e99765eeafcd82a2869939c89122ef09
somnath1077 <somnath.sikdar@gmail.com>
2020-06-11 14:35:20 +0200

    rational package

---
+++ src/main/scala/section5/rational.scala
@@ -0,0 +1,30 @@
+package section5
+
+class Rational(x: Int, y: Int) {
+    def numer = x
+
+    def denom = y
+
+    override def toString: String = x + "/" + y
+    
+    def isZero() = if (x == 0) true else false
+
+    def add(that: Rational) = {
+        val num = x * that.denom + y * that.numer
+        val den = y * that.denom
+        new Rational(num, den)
+    }
+
+    def neg() = {
+        new Rational(-1 * x, y)
+    }
+
+    def sub(that: Rational) = {
+        this.add(that.neg())
+    }
+
+    def div(that: Rational) = {
+        if (that.isZero()) throw new ArithmeticException("Divide by 0")
+        else new Rational(x * that.denom, y * that.numer)
+    }
+}
