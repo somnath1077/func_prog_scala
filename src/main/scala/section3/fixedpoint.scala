3553ff6ac3b4892dfff76e05cd0709c0e12d0225
somnath1077 <somnath.sikdar@gmail.com>
2020-06-11 13:50:29 +0200

    averageDamp

--- src/main/scala/section3/fixedpoint.scala
+++ src/main/scala/section3/fixedpoint.scala
@@ -2,25 +2,28 @@ package section3
 
 
 object fixedpoint {
-  def isCloseEnough(d1: Double, d2: Double): Boolean = {
-    def abs(x: Double): Double = if (x >= 0) x else -1 * x
+    def isCloseEnough(d1: Double, d2: Double): Boolean = {
+        def abs(x: Double): Double = if (x >= 0) x else -1 * x
 
-    val tol: Double = 1e-8
+        val tol: Double = 1e-8
 
-    if (abs((d2 - d1) / d1) < tol) true
-    else false
-  }
-
-  def fixedPoint(f: Double => Double)(firstGuess: Double): Double = {
-    def iterate(guess: Double): Double = {
-      println("guess = " + guess)
-      val next = f(guess)
-      if (isCloseEnough(guess, next)) next
-      else iterate(next)
+        if (abs((d2 - d1) / d1) < tol) true
+        else false
     }
 
-    iterate(firstGuess)
-  }
+    def fixedPoint(f: Double => Double)(firstGuess: Double): Double = {
+        def iterate(guess: Double): Double = {
 
-  def sqrt(x: Double) = fixedPoint(y => (y + x / y) / 2.0 )(1.0)
+            println("guess = " + guess)
+            val next = f(guess)
+            if (isCloseEnough(guess, next)) next
+            else iterate(next)
+        }
+
+        iterate(firstGuess)
+    }
+
+    def averageDamp(f: Double => Double)(x: Double) = (x + f(x)) / 2.0
+
+    def sqrt(x: Double) = fixedPoint(averageDamp(y => x / y))(1.0)
 }
