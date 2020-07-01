14e5fb651286d40be48cff274b6c4bfeb4bc7b54
Sikdar Somnath Dr <somnath.sikdar@metronom.com>
2020-07-01 17:21:35 +0200

    unused import

--- src/main/scala/section6/HigherOrderListFunctions.scala
+++ src/main/scala/section6/HigherOrderListFunctions.scala
@@ -1,32 +1,30 @@
 package section6
 
-import scala.annotation.tailrec
-
 object HigherOrderListFunctions {
-    def squareList(xs: List[Int]): List[Int] = xs match {
-        case List() => xs
-        case x :: xs1 => x * x :: squareList(xs1)
-    }
+  def squareList(xs: List[Int]): List[Int] = xs match {
+    case List() => xs
+    case x :: xs1 => x * x :: squareList(xs1)
+  }
 
-    def squareList2(xs: List[Int]): List[Int] = xs map (x => x * x)
+  def squareList2(xs: List[Int]): List[Int] = xs map (x => x * x)
 
-    def pack[T](xs: List[T]): List[List[T]] = xs match {
-        case Nil => Nil
-        case x :: xs1 =>
-            val (first, last) = xs span (y => y == x)
-            first :: pack(last)
-    }
+  def pack[T](xs: List[T]): List[List[T]] = xs match {
+    case Nil => Nil
+    case x :: xs1 =>
+      val (first, last) = xs span (y => y == x)
+      first :: pack(last)
+  }
 
-    def encode[T](xs: List[T]): List[(T, Int)] = xs match {
-        case Nil => Nil
-        case x :: xs1 =>
-            val (first, last) = xs span (y => y == x)
-            (x, first.length) :: encode(last)
-    }
+  def encode[T](xs: List[T]): List[(T, Int)] = xs match {
+    case Nil => Nil
+    case x :: xs1 =>
+      val (first, last) = xs span (y => y == x)
+      (x, first.length) :: encode(last)
+  }
 
 
-    def concat[T](xs: List[T], ys: List[T]): List[T] = xs match {
-        case Nil => ys
-        case x :: xs1 => x :: concat(xs1, ys)
-    }
+  def concat[T](xs: List[T], ys: List[T]): List[T] = xs match {
+    case Nil => ys
+    case x :: xs1 => x :: concat(xs1, ys)
+  }
 }
